002525874be286bec89257cdaf15ff307fd85205
rustytools <rustytools@users.noreply.github.com>
2017-05-13 15:15:13 +0000

    readme added

---
+++ README.md
@@ -0,0 +1,36 @@
+Error location utility macros
+=============================
+
+This project containst two utility macros and a function
+that can make the use of `std::panic::catch_unwind` more convenient.
+
+Plese note, that stack unwinding is not an idiomatic Rust error handling
+(but may be useful nonetheless).
+
+Usage example
+-------------
+
+```rust
+    #[macro_use]
+    extern crate errloc_macros;
+
+    std::panic::catch_unwind(|| {
+		foo.api_call_that_shoud_not_fail.expect(errloc!());
+	}).unwrap_or_else(|e| {
+        // output will include the location of "expect" call
+		println!("Fatal fail: {:?}", errloc_macros::msg(&e));
+	});
+```
+
+License information
+-------------------
+
+This project is released under the [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0).
+
+Changelog
+---------
+
+**2017-05-13**
+
+ * version 0.1.0
+ * initial public version
--- tests/test.rs
+++ tests/test.rs
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017, https://github.com/rustytools
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #[macro_use]
 extern crate errloc_macros;
@@ -5,8 +20,8 @@ extern crate errloc_macros;
 #[test]
 fn test_loc() {
     // note: checks are sensitive to line numbers
-    assert_eq!("tests/test.rs:8", errloc!());
-    assert_eq!("Hello! (at tests/test.rs:9)", errlocm!("Hello!"));
+    assert_eq!("tests/test.rs:23", errloc!());
+    assert_eq!("Hello! (at tests/test.rs:24)", errlocm!("Hello!"));
 }
 
 #[test]
